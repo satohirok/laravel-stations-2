af42a714ce36180d12cb9b829d18a75ef9957444
satohirok <hiroki_g0421@yahoo.co.jp>
2024-07-12 07:20:02 +0900

    st12: テーブルにgenreカラム追加

--- database/migrations/2024_07_03_075042_create_movies_table.php
+++ database/migrations/2024_07_03_075042_create_movies_table.php
@@ -15,11 +15,13 @@ class CreateMoviesTable extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->uniqid()->comment('映画タイトル');
+            $table->string('title')->unique()->comment('映画タイトル');
             $table->text('image_url')->comment('画像URL');
             $table->integer('published_year')->comment('公開年');
             $table->tinyInteger('is_showing')->default(false)->comment('公開年');
             $table->text('description')->comment('概要');
+            $table->unsignedBigInteger('genre_id');
+            $table->foreign('genre_id')->references('id')->on('genres');
             $table->timestamps();
         });
     }
