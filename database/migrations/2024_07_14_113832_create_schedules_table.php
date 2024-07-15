ba2045a4a0d6a3128021e414e6d4a8facb9cd5cf
satohirok <hiroki_g0421@yahoo.co.jp>
2024-07-15 09:42:44 +0900

    st14: scheduelテーブル作成

---
+++ database/migrations/2024_07_14_113832_create_schedules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('movie_id');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->timestamps();
+
+            $table->foreign('movie_id')->references('id')->on('movies');
+            $table->index('movie_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
