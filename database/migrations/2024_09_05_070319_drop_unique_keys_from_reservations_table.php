2edb6c947b50dda976427a775b48fc68c71badca
satohirok <hiroki_g0421@yahoo.co.jp>
2024-09-05 07:17:48 +0900

    st20 feat: unique制約の追加 & 削除

--- database/migrations/2024_09_05_070319_drop_unique_keys_from_reservations_table.php
+++ database/migrations/2024_09_05_070319_drop_unique_keys_from_reservations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateReservationsTableUniqueConstraints extends Migration
+class DropUniqueKeysFromReservationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class UpdateReservationsTableUniqueConstraints extends Migration
      */
     public function up()
     {
-        //
         Schema::table('reservations', function (Blueprint $table) {
-            // 新しいユニーク制約を追加
-            $table->unique(['schedule_id', 'sheet_id', 'screen_id']);
+             // UNIQUE KEY `reservations_schedule_id_sheet_id_unique` を削除
+             $table->dropUnique('reservations_schedule_id_sheet_id_unique');
+
+             // UNIQUE KEY `reservations_screen_id_sheet_id_unique` を削除
+             $table->dropUnique('reservations_screen_id_sheet_id_unique');
         });
     }
 
@@ -27,11 +29,11 @@ class UpdateReservationsTableUniqueConstraints extends Migration
      */
     public function down()
     {
-        //
         Schema::table('reservations', function (Blueprint $table) {
+            // UNIQUE KEY `reservations_schedule_id_sheet_id_unique` を復元
             $table->unique(['schedule_id', 'sheet_id'], 'reservations_schedule_id_sheet_id_unique');
 
-
+            // UNIQUE KEY `reservations_screen_id_sheet_id_unique` を復元
             $table->unique(['screen_id', 'sheet_id'], 'reservations_screen_id_sheet_id_unique');
         });
     }
